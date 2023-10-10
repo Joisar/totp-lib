281dd81661aaf84ff69bef0c2440a9261e92679f
webdev1 <webdev1@digicians.com>
2023-10-10 09:51:55 +0530

    85zu37vg7: config change

--- src/Totp.php
+++ src/Totp.php
@@ -12,12 +12,13 @@ class Totp
             $secret_key = Config::get('app.key');
         }
         $message = floor(time() / $time);
-        $hash = hash_hmac($algorithm, Totp . phppack('N', $message) . $email, $secret_key, true);
+        $hash = hash_hmac($algorithm, pack('N', $message) . $email, $secret_key, true);
         $offset = ord(substr($hash, -1)) & 0x0F;
         $truncatedHash = substr($hash, $offset, 4);
         $truncatedHash = $truncatedHash & "\x7F\xFF\xFF\xFF";
         $code = unpack('N', $truncatedHash)[1] % 1000000;
         return str_pad($code, $digits, '0', STR_PAD_LEFT);
+
     }
 
 }
